07c84704e262f213eb1821a5d456ba96e7b510cf
Daria Vikhoreva <darja.vihoreva@yandex.ru>
2023-02-12 00:41:12 +0400

    added axios

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 'use strict';
+import axios from 'axios';
 
 window.addEventListener('DOMContentLoaded', () => {
     // Tabs
@@ -196,9 +197,9 @@ window.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     }
     
-    getResource('http://localhost:3000/menu')
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
             });
         });
